b0dfcecec5e45f19681032dcb9a99e4997eda611
sumit <sumitkumar.nayak@tmtconnekt.com>
2021-06-04 10:49:43 +0530

    Prod to Dev

--- mis/settings/production.py
+++ mis/settings/production.py
@@ -1,6 +1,5 @@
 from .base import *
 
 # DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1','partners.protect4less.com','stg.partners.protect4less.com', 'stgpartners.protect4less.com']
-
-
