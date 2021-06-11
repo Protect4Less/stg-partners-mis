1fb8f28669701cc42540165538fc5d879f601ab0
support <support@protect4less.com>
2021-06-11 14:19:32 +0530

    MILES

--- policy/urls.py
+++ policy/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('brand-model', views.get_brand_model_ajax, name='brand-model'),
     path('get-model', views.get_model_ajax, name='get-model'),
     path('get-plan-price', views.get_plan_price_ajax, name='get-plan-price'),
-    path('get-catid', views.get_cat_id_ajax, name='get-catid'),
+    path('get-catid/', views.get_cat_id_ajax, name='get-catid'),
 ]
