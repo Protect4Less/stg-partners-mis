96825823d4fb110fbb747ec641d2da37f3dd306e
p4l <support@protect4less.com>
2021-07-14 12:15:58 +0530

    bulk_upload_bsquaredwifi

--- policy/views.py
+++ policy/views.py
@@ -218,7 +218,7 @@ def bulk_upload(request):
             cnt = cnt + 1
 
     context = {}
-    partner_code = 1040
+    partner_code = '1040'
     bsquaredwifi_data = helper_get_bsquaredwifi_data(request,partner_code)
     print('bsquaredwifi_data==',bsquaredwifi_data)
     context = {'bsquaredwifi_data':bsquaredwifi_data, 'partner_code':partner_code}
